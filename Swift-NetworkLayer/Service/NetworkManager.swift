b9e5a0473b7963f50a558ebb155b66cdfe3f6acf
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2023-08-18 14:13:35 +0300

    Initial Commit

---
+++ Swift-NetworkLayer/Service/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Swift-NetworkLayer
+//
+//  Created by Batuhan Kucukyildiz on 18.08.2023.
+//
+
+import Foundation
---
+++ Swift-NetworkLayer/Swift_NetworkLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_NetworkLayerApp.swift
+//  Swift-NetworkLayer
+//
+//  Created by Batuhan Kucukyildiz on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_NetworkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
