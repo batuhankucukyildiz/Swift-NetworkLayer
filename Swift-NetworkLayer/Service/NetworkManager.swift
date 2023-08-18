60c5165a2869dc26b99825bbe6c7b2a568912d29
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2023-08-18 16:37:50 +0300

    network manager add

--- Swift-NetworkLayer/Service/NetworkManager.swift
+++ Swift-NetworkLayer/Service/NetworkManager.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+
+class NetworkManager{
+    static let shared = NetworkManager()
+    
+    func request <T : Decodable>(_ endpoint : Endpoint ,completion : @escaping (Result<T? , Error>) -> Void ) -> Void {
+        let networkManagerTask = URLSession.shared.dataTask(with: endpoint.request()) {data ,response , error in
+            
+            if let error = error{
+                completion(.failure(error))
+                return
+            }
+            guard let response = response as? HTTPURLResponse , response.statusCode >= 200 , response.statusCode <= 500 else{
+                completion(.failure(NSError(domain: "Invalid Error", code: 0)))
+                return 
+            }
+            guard let data = data else {
+                completion(.failure(NSError(domain: "Invalid Error", code: 0)))
+                return
+            }
+            do{
+                let decodedData = try? JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedData))
+            }catch let error {
+                completion(.failure(error))
+            }
+        }
+        networkManagerTask.resume()
+    }
+    
+    func getUser(){
+        //
+    }
+    
+}
